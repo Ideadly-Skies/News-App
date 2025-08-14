b24921822d69f6897d39a349e5b53ef572c6a7af
Ideadly-Skies <Obie.kal22@gmail.com>
2025-08-14 21:54:25 +1000

    feat(loading): refactored Loading text with LoadingAnimation from Json Lottie

--- news-app/src/app/loading.tsx
+++ news-app/src/app/loading.tsx
@@ -1,7 +1,11 @@
+"use client"
+import Lottie from "lottie-react";
+import loadingAnimation from "@/lotties/Material wave loading.json";
+
 export default function Loading() {
   return (
     <div className="min-h-[calc(100vh-theme(spacing.32))] w-full flex justify-center items-center">
-      <h1 className="text-xl">Loading...</h1>
+      <Lottie animationData={loadingAnimation} loop={true} style={{ width: 200, height: 200 }} />
     </div>
   );
 }
